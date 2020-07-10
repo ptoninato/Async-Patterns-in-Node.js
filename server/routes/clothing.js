8b00a92682abe5bbb27e48f899d02918d3d012c4
ptoninato <pmtoninato@gmail.com>
2020-07-10 07:10:58 -0500

    promise refactor

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -6,25 +6,28 @@ const router = express.Router();
 /* GET all clothing */
 router.route('/')
   .get(function(req, res) {
-    getClothingData((err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Returning Clothing Data');
+    
+    getClothingData()
+      .then(data => {
+        console.log('Returning clothing data to brownser.');
         res.send(data);
-      }
-    });
+      })
+      .catch(error => res.status(500).send(error))
+      .finally(() => console.log('All done processing promise'));
+
     console.log('Doing more work');
   });
 
-function getClothingData(callback) {
-  fs.readFile(datafile, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      let clothingData = JSON.parse(data);
-      callback(null, clothingData);
-    }
+function getClothingData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(datafile, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let clothingData = JSON.parse(data);
+        resolve(clothingData);
+      }
+    });
   });
 }
 
